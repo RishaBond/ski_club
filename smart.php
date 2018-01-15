6ddaf89549ae134762c9cca9f5df09ef9ddcf1b3
Irina Bondarenko <lavless13@mail.ru>
2018-01-15 19:59:20 +0300

    123

---
+++ smart.php
@@ -0,0 +1,41 @@
+<?php 
+$whatever = $POST([wharever]);
+$user_name = $POST([user_name]);
+$user_phone = $POST([user_phone]);
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'mr.vaskyapupkin@mail.ru';                 // SMTP username
+$mail->Password = 'P4oD6z*s=BYN-X1X?9';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+
+$mail->setFrom('mr.vaskyapupkin@mail.ru', 'Pupkin V.');
+$mail->addAddress('lavless13@mail.ru', 'Joe User');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка на онлайн бронировани';
+$mail->Body    = 'Это тело сообщения дальше жирное <b>блаблабла</b>';
+$mail->AltBody = 'Это сообщение в формате plain text';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+
+?>
